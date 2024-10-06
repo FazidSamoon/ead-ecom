cab5855ac96733334e6c3a9c3bdecd198d290bce
FazidSamoon <tuan.fazid@outlook.com>
2024-10-06 23:24:29 +0530

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,19 @@ import Home from "./HomePage/Home";
 import Login from "./LoginPage/Login";
 import Signup from "./SignupPage/Signup";
 import Layout from "../components/organisms/layout/Layout";
-import UnAuthorized from "../components/organisms/unauthorized/unAuthorized";
 import UserManagementHome from "./UserManagementViews/UserManagementHome/UserManagementHome";
 import { useEffect } from "react";
 import ProductHome from "./ProductHome/ProductHome";
 import OrderHome from "./OrderHome/OrderHome";
 import InventoryHome from "./InventoryHome/InventoryHome";
 import VendorHome from "./VendorHome/VendorHome";
+import UnAuthorized from "../components/organisms/unauthorized/UnAuthorized"
 
 const index = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const navigate = useNavigate();
   const isAdmin = localStorage.getItem("role") == "admin";
   const user = localStorage.getItem("user");
-
-  console.log(isAdmin)
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
@@ -41,11 +39,11 @@ const index = () => {
               />
               <Route
                 path="/products/"
-                element={user && isAdmin ? <ProductHome /> : <UnAuthorized />}
+                element={user ? <ProductHome /> : <UnAuthorized />}
               />
               <Route
                 path="/orders/"
-                element={user && isAdmin ? <OrderHome /> : <UnAuthorized />}
+                element={user ? <OrderHome /> : <UnAuthorized />}
               />
               <Route
                 path="/inventory/"
@@ -60,7 +58,7 @@ const index = () => {
             <>
               <Route path="auth">
                 <Route path="login"register element={<Login />} />,
-                <Route path="" element={<Signup />} />
+                <Route path="signup" element={<Signup />} />
               </Route>
             </>
           )}
